57a3a9c403a923fb5d640b5b892379947f4ef4f6
jakob <jakob.spiess@stud.unibas.ch>
2025-05-05 19:48:29 +0200

    Commit before I will change all java files back to SQL since probably I will have to do it with the dialect substitutions and then work from there (add mql adapter and so on)

--- src/main/java/com/oltpbenchmark/benchmarks/ycsb/procedures/DeleteRecord.java
+++ src/main/java/com/oltpbenchmark/benchmarks/ycsb/procedures/DeleteRecord.java
@@ -3,23 +3,17 @@ package com.oltpbenchmark.benchmarks.ycsb.procedures;
 import com.oltpbenchmark.api.Procedure;
 import com.oltpbenchmark.api.SQLStmt;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DeleteRecord extends Procedure {
 
-  // Placeholder statement name to be overridden by the dialect XML
-  public final SQLStmt deleteStmt = new SQLStmt("DIALECT_PLACEHOLDER");
+  public final SQLStmt deleteStmt = new SQLStmt("db.usertable.deleteOne({ YCSB_KEY: ? })");
 
   public void run(Connection conn, int keyname) throws SQLException {
-    // Get the statement from the dialect file
-    final String stmtStr = getPreparedStatement("deleteStmt");
-
-    // Replace '?' with the actual key value
-    final String resolvedStmt = stmtStr.replace("?", String.valueOf(keyname));
-
-    try (Statement stmt = conn.createStatement()) {
-      stmt.executeUpdate(resolvedStmt);
+    try (PreparedStatement stmt = this.getPreparedStatement(conn, deleteStmt)) {
+      stmt.setInt(1, keyname);
+      stmt.executeUpdate();
     }
   }
 }
