7c842d76324a2f847ae66b1b5b326b302766728f
jakob <jakob.spiess@stud.unibas.ch>
2025-05-05 19:50:25 +0200

    InsertRecord.java changed back to SQL

--- src/main/java/com/oltpbenchmark/benchmarks/ycsb/procedures/InsertRecord.java
+++ src/main/java/com/oltpbenchmark/benchmarks/ycsb/procedures/InsertRecord.java
@@ -1,4 +1,23 @@
+/*
+ * Copyright 2020 by OLTPBenchmark Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.oltpbenchmark.benchmarks.ycsb.procedures;
+
+import static com.oltpbenchmark.benchmarks.ycsb.YCSBConstants.TABLE_NAME;
 
 import com.oltpbenchmark.api.Procedure;
 import com.oltpbenchmark.api.SQLStmt;
@@ -8,11 +27,11 @@ import java.sql.SQLException;
 
 public class InsertRecord extends Procedure {
   public final SQLStmt insertStmt =
-      new SQLStmt(
-          "db.usertable.insertOne({ YCSB_KEY: ?, FIELD0: ?, FIELD1: ?, FIELD2: ?, FIELD3: ?, FIELD4: ?, FIELD5: ?, FIELD6: ?, FIELD7: ?, FIELD8: ?, FIELD9: ? })");
+      new SQLStmt("INSERT INTO " + TABLE_NAME + " VALUES (?,?,?,?,?,?,?,?,?,?,?)");
 
+  // FIXME: The value in ysqb is a byteiterator
   public void run(Connection conn, int keyname, String[] vals) throws SQLException {
-    try (PreparedStatement stmt = this.getPreparedStatement(conn, insertStmt)) {
+    try (PreparedStatement stmt = this.getPreparedStatement(conn, this.insertStmt)) {
       stmt.setInt(1, keyname);
       for (int i = 0; i < vals.length; i++) {
         stmt.setString(i + 2, vals[i]);
