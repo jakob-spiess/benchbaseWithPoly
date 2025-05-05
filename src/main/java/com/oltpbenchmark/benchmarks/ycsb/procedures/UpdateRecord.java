aec40446e26e9e061c14765d6e7c24a379ef01bb
jakob <jakob.spiess@stud.unibas.ch>
2025-05-05 19:53:56 +0200

    Change UpdateRecord.java back to SQL

--- src/main/java/com/oltpbenchmark/benchmarks/ycsb/procedures/UpdateRecord.java
+++ src/main/java/com/oltpbenchmark/benchmarks/ycsb/procedures/UpdateRecord.java
@@ -1,4 +1,23 @@
+/*
+ * Copyright 2020 by OLTPBenchmark Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.oltpbenchmark.benchmarks.ycsb.procedures;
+
+import static com.oltpbenchmark.benchmarks.ycsb.YCSBConstants.TABLE_NAME;
 
 import com.oltpbenchmark.api.Procedure;
 import com.oltpbenchmark.api.SQLStmt;
@@ -10,24 +29,18 @@ public class UpdateRecord extends Procedure {
 
   public final SQLStmt updateAllStmt =
       new SQLStmt(
-          "db.usertable.updateOne("
-              + "{ YCSB_KEY: ? }, "
-              + "{ $set: { FIELD1: ?, FIELD2: ?, FIELD3: ?, FIELD4: ?, FIELD5: ?, "
-              + "FIELD6: ?, FIELD7: ?, FIELD8: ?, FIELD9: ?, FIELD10: ? } })");
+          "UPDATE "
+              + TABLE_NAME
+              + " SET FIELD1=?,FIELD2=?,FIELD3=?,FIELD4=?,FIELD5=?,"
+              + "FIELD6=?,FIELD7=?,FIELD8=?,FIELD9=?,FIELD10=? WHERE YCSB_KEY=?");
 
   public void run(Connection conn, int keyname, String[] vals) throws SQLException {
-    // Get a prepared statement from the MQL string we wrote above
     try (PreparedStatement stmt = this.getPreparedStatement(conn, updateAllStmt)) {
 
-      // First parameter is the key — goes into: { YCSB_KEY: ? }
-      stmt.setInt(1, keyname);
-
-      // Next 10 parameters are the values to update — go into the $set part
+      stmt.setInt(11, keyname);
       for (int i = 0; i < vals.length; i++) {
-        stmt.setString(i + 2, vals[i]); // +2 because keyname was param 1
+        stmt.setString(i + 1, vals[i]);
       }
-
-      // Actually run the query
       stmt.executeUpdate();
     }
   }
